1e8c5c8ace394cdf01f03b5b8373164762aeed93
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-14 15:37:25 +0700

    Update show.php

--- show.php
+++ show.php
@@ -17,7 +17,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table class="table">
+<table class="table" width="600" border="1">
     <thead class="thead-dark">
       <tr>
         <th width="100"> <div align="center">Name</div></th>
