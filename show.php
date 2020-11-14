76ace5f0ffe8c3613bece41cd9786f34d49f4113
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-14 16:09:22 +0700

    Update show.php

--- show.php
+++ show.php
@@ -34,7 +34,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-    </tr>
+  </tr>
 <?php
 }
 ?>
