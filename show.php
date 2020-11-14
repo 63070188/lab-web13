ddf655193fbd6182ce11341b861bd946d3934362
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-14 19:39:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -22,9 +22,9 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <table class="table" width="600" border="5" bordercolor="white">
       <thead class="thead-dark">
         <tr>
-          <th> <div align="center">Name</div></th>
-          <th width="400"> <div align="center">Comment </div></th>
-          <th width="300"> <div align="center">Actions </div></th>
+          <th 10%> <div align="center">Name</div></th>
+          <th width=50%> <div align="center">Comment </div></th>
+          <th width=40%> <div align="center">Actions </div></th>
         </tr>
 </div>
 <?php
