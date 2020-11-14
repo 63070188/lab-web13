2c39c37cc1eac1936ba3d9cd645b5348d4e012b0
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-14 16:25:45 +0700

    Update show.php

--- show.php
+++ show.php
@@ -42,6 +42,8 @@ while($Result = mysqli_fetch_array($res))
 <?php
 mysqli_close($conn);
 ?>
-<button><b>ปุ่มแบบข้อความ</b></button>
+<div class="container">
+  <button><b>ปุ่มแบบข้อความ</b></button>
+</div>
 </body>
 </html>
