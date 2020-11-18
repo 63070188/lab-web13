e078efcc8127a34e3454e0fadd323344dd3040b7
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-19 00:15:35 +0700

    Delete dbconfig.php

--- new_itf/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'host' => 'lab-itf17.mysql.database.azure.com',
-    'username' => 'Aphatsara@lab-itf17',
-    'password' => 'Po0926245419',
-    'database' => 'ITFLab',
-);
-?>
