b842bfa439fb4c67d5c4e580846e9aaacdbcd45c
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-18 23:15:25 +0700

    Update dbconfig.php

--- new itf/dbconfig.php
+++ new itf/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'host' => 'host',
-    'username' => 'username',
-    'password' => 'password',
-    'database' => 'databasename',
+    'host' => 'lab-itf17.mysql.database.azure.com',
+    'username' => 'Aphatsara@lab-itf17',
+    'password' => 'Po0926245419',
+    'database' => 'ITFLab',
 );
 ?>
