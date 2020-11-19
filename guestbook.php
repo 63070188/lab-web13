609159037c7c90512b7cfb437ef467f7bc4cca4d
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-19 19:19:58 +0700

    Update guestbook.php

--- guestbook.php
+++ guestbook.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ITF Lab</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -20,7 +22,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 <div class="container">
     <h1>List</h1>
     <table class="table table-responsive-md">
-        <thead>
+        <thead class="thead-dark">
             <tr>
                 <th scope="col">Name</th>
                 <th scope="col">Comment</th>
